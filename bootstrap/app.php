a6cdde17df0b7815d969abcd26a5d992c00156ba
miraich <tweetandnews@gmail.com>
2024-05-17 13:55:09 +0300

    changed cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
 //        $exceptions->respond(function (Response $response) {
