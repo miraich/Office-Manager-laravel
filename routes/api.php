9920b5d61e15d1bba4248ddb1ee4f235fc867a4d
miraich <tweetandnews@gmail.com>
2024-05-17 15:49:57 +0300

    test verified middleware

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth:sanctum")->group(function () {
-    Route::get('/user', [UserController::class, "show"]);
+    Route::get('/user', [UserController::class, "show"])->middleware('verified');
     Route::get('/messages', [ChatController::class, 'show']);
     Route::post('/message', [MessageController::class, 'store']);
     Route::post('/task', [TaskController::class, "store"]);
