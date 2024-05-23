6904632feef763dd3b1c8ff66142a2dfe17d3a99
miraich <tweetandnews@gmail.com>
2024-05-23 16:45:15 +0300

    many changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('password');
             $table->string('email_verification_token');
-            $table->timestampTz('email_verified_at');
+            $table->timestampTz('email_verified_at')->nullable();
 //            $table->rememberToken();
             $table->timestampsTz();
         });
