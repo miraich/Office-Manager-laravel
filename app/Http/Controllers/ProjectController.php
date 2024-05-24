571de9d10e72f8c5fc586e175ec6988df91275df
miraich <tweetandnews@gmail.com>
2024-05-24 17:30:00 +0300

    added file uploading

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -72,15 +72,13 @@ class ProjectController extends Controller
     {
         $path = $project->documentation;
 
-        return response()->download(storage_path() . "/app/" . $path,'file',[
-            'Content-Type' => 'application/octet-stream',
-        ]);
+//        return response()->download(storage_path() . "/app/" . $path,'file',[
+//            'Content-Type' => 'application/octet-stream',
+//        ]);
 
-//        dd(Storage::download($path));
-
-//        if (!empty($path) && Storage::disk('local')->exists($path)) {
-//            return Storage::download($path);
-//        }
-//        return response()->json(['error' => 'File not found.'], 404);
+        if (!empty($path) && Storage::disk('local')->exists($path)) {
+            return Storage::download($path);
+        }
+        return response()->json(['error' => 'File not found.'], 404);
     }
 }
